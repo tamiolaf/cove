d3bbd154307d4cb9bdc08a8d3f98eea1e263d051
Tami Olafunmiloye <tamiolaf@gmail.com>
2023-04-18 20:53:56 -0400

    add gsmr video background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import animation from './animation.mp4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <section className="App-header">
+        <div className="gsmr-logo">GSMR</div>
+      </section>
+      <section>
+
+      <video id="vid" src={animation} controls autoPlay loop muted>
+      </video>
+      </section>
     </div>
   );
 }
